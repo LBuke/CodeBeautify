21e98ccbe64a018485a9910dfc5223baf84447b6
LBuke <lbingham1997@gmail.com>
2023-04-18 17:46:25 -0400

    images
    Signed-off-by: LBuke <lbingham1997@gmail.com>

--- events/ready.js
+++ events/ready.js
@@ -6,3 +6,12 @@ export const data = {
         bot.user.setPresence({ activities: [{ name: `${bot.guilds.cache.size} servers!`}] });
     }
 }
+
+function fizzbuzz() {
+    for (let i = 0; i < 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) console.log('FizzBuzz');
+        else if (i % 3 === 0) console.log('Fizz');
+        else if (i % 5 === 0) console.log('Buzz');
+        else console.log(i);
+    }
+}
