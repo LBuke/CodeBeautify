290dcd561db51ad14477feb9d3bf0a915126eebc
LBuke <lbingham1997@gmail.com>
2023-04-18 18:14:56 -0400

    dev
    Signed-off-by: LBuke <lbingham1997@gmail.com>

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ const bot = new Client({
 
 // Load Event files from events folder
 const eventFiles = fs.readdirSync('./events/').filter(f => f.endsWith('.js'))
-for (const file of eventFiles) {
-    console.log(`${file} loaded`)
-    const event = await import(`./events/${file}`)
-    if(event.data.once) {
-        bot.once(event.data.name, (...args) => event.data.execute(...args, bot))
-    } else {
-        bot.on(event.data.name, (...args) => event.data.execute(...args, bot))
+(async () => {
+    for (const file of eventFiles) {
+        console.log(`${file} loaded`)
+        const event = await import(`./events/${file}`)
+        if(event.data.once) {
+            bot.once(event.data.name, (...args) => event.data.execute(...args, bot))
+        } else {
+            bot.on(event.data.name, (...args) => event.data.execute(...args, bot))
+        }
     }
-}
+})()
 
 (async () => {
     await bot.login(config.token);
